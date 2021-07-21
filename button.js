fe7174a58fdf84e9d43c8f767a1b354635cbc20c
thunderjak <1026253290@qq.com>
2021-07-21 17:39:40 +0800

    npm装parcel报错，直接换yarn装

--- button.js
+++
@@ -1,5 +0,0 @@
-Vue.component('a-button',{
-    template:`
-    <button>hi</button>
-    `
-})
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from "vue";
+import Button from './Button'
+
+Vue.component('a-button',Button)
+
+new Vue({
+    el:'#app',
+})
