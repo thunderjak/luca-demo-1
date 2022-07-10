6accbb98d3b0656481f023449c856cee6bec790c
thunderjak <1026253290@qq.com>
2022-07-10 20:17:38 +0800

    检测button-group里的子元素

--- src/button-group.vue
+++ src/button-group.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="a-button-group"><slot></slot></div>
 </template>
-<script lang="ts"></script>
+<script lang="ts">
+export default {
+  mounted() {
+    for (let node of this.$el.children) {
+      let name = node.nodeName.toLowerCase();
+      if (name !== "button") {
+        console.warn(
+          `a-button-group 的子元素应该全是 a-button,但是你写的是 ${name}`
+        );
+      }
+    }
+  },
+};
+</script>
 
 <style lang="scss">
 .a-button-group {
